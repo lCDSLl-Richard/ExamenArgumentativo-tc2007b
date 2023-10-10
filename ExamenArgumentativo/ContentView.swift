8d46e1e6411a000b7083d745d7b30b93dce8b3bc
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2023-10-10 11:16:31 -0600

    Initial Commit

---
+++ ExamenArgumentativo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExamenArgumentativo
+//
+//  Created by Ricardo Adolfo Fernández Alvarado on 10/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExamenArgumentativo/ExamenArgumentativoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenArgumentativoApp.swift
+//  ExamenArgumentativo
+//
+//  Created by Ricardo Adolfo Fernández Alvarado on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ExamenArgumentativoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
