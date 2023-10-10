fe910e6ff88c17a7a7bfba46c63f51047392187b
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2023-10-10 11:36:52 -0600

    Added main view placeholder in clean architechture

---
+++ ExamenArgumentativo/Framework/Views/MainView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  ExamenArgumentativo
+//
+//  Created by Ricardo Adolfo Fernández Alvarado on 10/10/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text("Movie DB")
+        .font(.largeTitle)
+        .fontWeight(.bold)
+        .padding(.horizontal)
+      Spacer()
+      List {
+        MovieElementView()
+        MovieElementView()
+        MovieElementView()
+        MovieElementView()
+        MovieElementView()
+      }
+    }
+    .frame(maxWidth: .infinity)
+  }
+}
+
+struct MovieElementView: View {
+  var body: some View {
+    HStack {
+      Image(systemName: "person")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .padding()
+        .frame(width: 100, height: 200)
+      Spacer()
+      Text("Hey")
+      Spacer()
+    }
+  }
+}
+
+#Preview {
+  MainView()
+}
